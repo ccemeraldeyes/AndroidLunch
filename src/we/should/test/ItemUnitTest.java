5afecd257e18633fa6077f29fd22a8c65b737e18
Davis Shepherd <dgshep@gmail.com>
2012-04-24 09:43:49 -0700

    Added Basic Test

---
+++ src/we/should/test/ItemUnitTest.java
@@ -0,0 +1,22 @@
+package we.should.test;
+
+import junit.framework.TestCase;
+
+public class ItemUnitTest extends TestCase {
+
+	public ItemUnitTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	public void testGet() {
+		assertTrue(2==2);
+	}
+
+}
