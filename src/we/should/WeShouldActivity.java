c9de15a19a93259111b50a363fa7b1fdb421cc82
larrytj <lawrencetjok@hotmail.com>
2012-04-28 15:58:33 -0700

    map enabled to test phone, auto focus user when map load and probably when user moves

--- src/we/should/WeShouldActivity.java
+++ src/we/should/WeShouldActivity.java
@@ -1,5 +1,8 @@
 package we.should;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.content.Context;
 import android.location.Criteria;
 import android.location.Location;
@@ -14,26 +17,25 @@ import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
-
-
+import com.google.android.maps.MyLocationOverlay;
 public class WeShouldActivity extends MapActivity implements LocationListener{
 	
 	/** The TabHost that cycles between categories. **/
 	private TabHost mTabHost;
 	private MapView map;
 	private LocationManager lm;
-	private int devX, devY;
 	private MapController controller;
 	private String towers;
-	
+	private int devX, devY;
+	private MyLocationOverlay myLocationOverlay;
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         map = (MapView) findViewById(R.id.mapview);
-        map.setBuiltInZoomControls(true);
-        
+        map.setBuiltInZoomControls(true);       
         controller = map.getController();
         
         this.mTabHost = (TabHost) findViewById(android.R.id.tabhost);
@@ -44,19 +46,16 @@ public class WeShouldActivity extends MapActivity implements LocationListener{
         // pittsw: 4/20/12
         TabHost.TabSpec spec;
         View view;
-        
         spec = mTabHost.newTabSpec("restaurants").setIndicator("  Restaurants  ")
         		.setContent(android.R.id.list);
         mTabHost.addTab(spec);
-
         spec = mTabHost.newTabSpec("movies").setIndicator("  Movies  ")
         		.setContent(android.R.id.list);
         mTabHost.addTab(spec);
-        
         spec = mTabHost.newTabSpec("other").setIndicator("  Other  ")
         		.setContent(android.R.id.list);
         mTabHost.addTab(spec);
-
+        
         for (int i = 0; i < 10; i++) {
 	        spec = mTabHost.newTabSpec("other").setIndicator("  Other " + i + "  ")
 	        		.setContent(android.R.id.list);
@@ -65,6 +64,10 @@ public class WeShouldActivity extends MapActivity implements LocationListener{
 
         mTabHost.setCurrentTab(0);
         
+        //Testing
+        myLocationOverlay = new MyLocationOverlay(this, map);
+        map.getOverlays().add(myLocationOverlay);
+        //Getting current location.
         lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE); // it is a string
         Criteria crit = new Criteria();
         towers = (lm.getBestProvider(crit, false)); //getting best provider.
@@ -74,37 +77,51 @@ public class WeShouldActivity extends MapActivity implements LocationListener{
 	        devY = (int) (location.getLongitude() * 1E6);
 	        GeoPoint ourLocation = new GeoPoint(devX, devY);
 	        controller.animateTo(ourLocation);
-	        controller.setZoom(6);
+	        controller.setZoom(18);
 	        
         } else {
         	Toast.makeText(WeShouldActivity.this, 
         			"Couldn''t get provider", Toast.LENGTH_SHORT).show();
         }
+        map.postInvalidate();
+
         
     }
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		myLocationOverlay.disableMyLocation();
+		lm.removeUpdates(this);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		myLocationOverlay.enableMyLocation();
+		lm.requestLocationUpdates(towers, 500, 1, this);
+	}
+
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+		devX = (int) (location.getLatitude() * 1E6);
+		devY = (int) (location.getLongitude() * 1E6);
+		GeoPoint ourLocation = new GeoPoint(devX, devY);
+		controller.animateTo(ourLocation);
+	}
+
+	
+	//We will handle this later.
+	public void onProviderDisabled(String provider) {		
+	}
+	public void onProviderEnabled(String provider) {
+	}
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+	}
 
 	@Override
 	protected boolean isRouteDisplayed() {
 		return false;
 	}
-
-	public void onLocationChanged(Location arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void onProviderDisabled(String provider) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void onProviderEnabled(String provider) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-		// TODO Auto-generated method stub
-		
-	}
+    
 }
