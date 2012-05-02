daeeeb7b3850ed10dac0a32f9dfc473e7b2b0968
Troy <troyschuring@hotmail.com>
2012-05-02 08:30:41 -0700

    update db comments

--- src/we/should/database/ItemConst.java
+++ src/we/should/database/ItemConst.java
@@ -9,7 +9,7 @@ public class ItemConst {
 	
 	public static final int 	DATABASE_VERSION=1;
 	public static final String 	DATABASE_NAME="WeShould.db", // filename on device
-								TBL_NAME="item", 		 // table name
+								TBL_NAME="item", 			 // table name
 								ID="id",                     // unique Id
 								NAME="name",                 // item name
 								CAT_ID="category_id",        // references Category Id
