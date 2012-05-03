01dc7fb7680da07011dd62e310de570af5b50e3b
Davis Shepherd <dgshep@gmail.com>
2012-05-03 15:27:41 -0700

    Added more comments and checkreps

--- src/we/should/list/GenericCategory.java
+++ src/we/should/list/GenericCategory.java
@@ -2,16 +2,36 @@ package we.should.list;
 
 import java.util.*;
 
-
+/**
+ * 
+ * @author Davis
+ * 
+ * Representation Invariants:
+ * 
+ * For all Items i in items{
+ * 		i.category == this
+ * }
+ */
 public class GenericCategory extends Category {
 	private List<Field> fields;
 	private List<Item> items; 
+	
 	
 	public GenericCategory(String name, List<Field> fields) {
 		super(name);
 		this.fields = fields;
 		this.items = new ArrayList<Item>();
+		checkRep();
 		
+	}
+	/**
+	 * Checks the representation invariant.
+	 * Throws an assertion error if it is violated.
+	 */
+	private void checkRep(){
+		for(Item i : items){
+			assert(i.getCategory() == this);
+		}
 	}
 	@Override
 	public Set<Item> getItems() {
@@ -27,7 +47,9 @@ public class GenericCategory extends Category {
 	
 	@Override
 	protected boolean addItem(Item i) {
-		return this.items.add(i);
+		Boolean output = this.items.add(i);
+		checkRep();
+		return output;
 	}
 	@Override
 	public List<Field> getFields(){
