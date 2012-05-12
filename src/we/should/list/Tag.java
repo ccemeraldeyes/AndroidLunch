10af1bb3a83cf378a3b9ea8f48c6e70470d49e78
Davis Shepherd <dgshep@gmail.com>
2012-05-11 17:05:21 -0700

    Comments added to tag

--- src/we/should/list/Tag.java
+++ src/we/should/list/Tag.java
@@ -1,12 +1,9 @@
 package we.should.list;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import we.should.database.WSdb;
-
 import android.content.Context;
 import android.database.Cursor;
 
@@ -17,19 +14,41 @@ public class Tag {
 	private int id;
 	private String tag;
 	
+	/**
+	 * Creates a new tag object with the given name and DB id
+	 * @param id
+	 * @param tag
+	 */
 	public Tag(int id, String tag){
 		this.id = id;
 		this.tag = tag;
 	}
+	/**
+	 * Returns the name of this tag
+	 */
 	public String toString(){
 		return tag;
 	}
+	/**
+	 * Returns the DB row entry of this tag. 0 if it is
+	 * not saved to the DB.
+	 * @return this.id
+	 */
 	public int getId(){
 		return this.id;
 	}
+	/**
+	 * Sets the db id of this tag
+	 * @param i - should be the return value of a db insert call
+	 */
 	public void setId(int i){
 		this.id = i;
 	}
+	/**
+	 * Returns a list of all of the tags added to the DB
+	 * @param ctx of the database
+	 * @return a list of tag objects pulled from the DB
+	 */
 	public static List<Tag> getTags(Context ctx){
 		if(ctx == null){
 			throw new IllegalArgumentException("Context cannot be null!");
@@ -45,12 +64,16 @@ public class Tag {
 		}
 		return out;	
 	}
+	/**
+	 * returns true if the names of this and o are the same.
+	 */
 	public boolean equals(Object o){
 		if(o == this) return true;
 		if(o == null || !(o instanceof Tag)) return false;
 		Tag cp= Tag.class.cast(o);
 		return this.tag.equals(cp.tag);
 	}
+	
 	public int hashCode(){
 		return this.tag.hashCode();
 	}
