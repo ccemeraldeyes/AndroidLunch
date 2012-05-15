8a9cc552eab8f8b08941748b886392fcb2218333
Colleen Ross <colleen@collegebrain.com>
2012-05-14 22:45:19 -0700

    code for on-save backup

--- src/we/should/EditScreen.java
+++ src/we/should/EditScreen.java
@@ -1,14 +1,29 @@
 package we.should;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import we.should.list.Category;
 import we.should.list.Field;
 import we.should.list.Item;
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -79,6 +94,40 @@ public class EditScreen extends Activity {
 		}
 		
 		mItem.save();
+		
+		Pattern emailPattern = Patterns.EMAIL_ADDRESS; // API level 8+
+		Account[] accounts = AccountManager.get(getBaseContext()).getAccounts();
+		String email = "";
+		for (Account account : accounts) {
+		    if (emailPattern.matcher(account.name).matches()) {
+		        email = account.name;
+		        break;
+		    }
+		}
+		
+		// Create a new HttpClient and Post Header
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httppost = new HttpPost("http://23.23.237.174/save-item");
+
+		try {
+		    // Add your data
+		    List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+		    nameValuePairs.add(new BasicNameValuePair("user_email", email));
+			for (Field f : mData.keySet()) {
+				nameValuePairs.add(new BasicNameValuePair(f.getName(), mData.get(f)));
+			}
+		    httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+		    // Execute HTTP Post Request
+		    HttpResponse response = httpclient.execute(httppost);
+
+		} catch (ClientProtocolException e) {
+		    // TODO Auto-generated catch block
+		} catch (IOException e) {
+		    // TODO Auto-generated catch block
+		}
+		
+		
 		finish();
 	}
 	
