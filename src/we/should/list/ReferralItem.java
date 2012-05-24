6bd43244bffe1f50008cef35397a5b4f8040e7a8
Davis Shepherd <dgshep@gmail.com>
2012-05-23 18:07:19 -0700

    Added field ordering

--- src/we/should/list/ReferralItem.java
+++ src/we/should/list/ReferralItem.java
@@ -1,5 +1,6 @@
 package we.should.list;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -19,7 +20,7 @@ import android.content.Context;
 public class ReferralItem extends GenericItem {
 	
 	private List<Field> fields;
-	
+
 
 	/**
 	 * Creates a new Referral Item from the JSON object passed to it
@@ -42,6 +43,17 @@ public class ReferralItem extends GenericItem {
 	@Override
 	public List<Field> getFields(){
 		return this.fields;
+	}
+	protected void DBtoData(JSONObject d) throws JSONException{
+		this.fields = new LinkedList<Field>();
+		super.DBtoData(d);
+		@SuppressWarnings("unchecked")
+		Iterator<String> keys = d.keys();
+		String k;
+		while(keys.hasNext()){
+			k = keys.next();
+			fields.add(new Field(k));
+		}
 	}
 	
 
