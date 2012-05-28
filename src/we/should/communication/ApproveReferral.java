8ea285559126e105d8aff9af89d9733852d15189
Colleen Ross <colleen@collegebrain.com>
2012-05-27 19:38:29 -0700

    deleting referrals works

--- src/we/should/communication/ApproveReferral.java
+++ src/we/should/communication/ApproveReferral.java
@@ -1,13 +1,23 @@
 package we.should.communication;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import we.should.R;
+import we.should.WeShouldActivity;
 import we.should.database.WSdb;
 import we.should.list.Category;
 import we.should.list.Item;
@@ -16,6 +26,7 @@ import we.should.list.Referrals;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -40,7 +51,7 @@ public class ApproveReferral extends Activity {
 		setContentView(R.layout.approve_referral);
 		
 		ListView lv = (ListView) findViewById(R.id.referralList);
-		List<Referral> list = new ArrayList<Referral>();
+		final List<Referral> list = new ArrayList<Referral>();
 		Intent intent = this.getIntent();
 		Bundle bundle = intent.getExtras();
 		String dataAsString = bundle.getString("data");
@@ -101,10 +112,60 @@ public class ApproveReferral extends Activity {
 
 				}
 				
+				deleteRefs(list);
+				
 				finish();
 			}
 			
 		});
+	}
+	
+	public void deleteRefs(List<Referral> referrals){
+		
+		Log.v("DELETE REFERRALS", "deleting");
+		SharedPreferences settings = getSharedPreferences(WeShouldActivity.PREFS, 0);
+		
+		HttpClient httpclient = new DefaultHttpClient();
+		
+	    List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+	    nameValuePairs.add(new BasicNameValuePair("user_email", settings.getString(WeShouldActivity.ACCOUNT_NAME, "")));
+		
+	    JSONArray listArray = new JSONArray();
+	    
+	    for(Referral r: referrals){
+	    	JSONObject o = new JSONObject();
+	    	try {
+				o.put("name", r.getName());
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    	try {
+				o.put("referred_by", r.getSender());
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    	listArray.put(o);
+	    }
+	    
+	    nameValuePairs.add(new BasicNameValuePair("delete_list", listArray.toString()));
+	    
+		String paramString = URLEncodedUtils.format(nameValuePairs, "utf-8");
+		
+		HttpGet httpget = new HttpGet("http://23.23.237.174/delete-referrals?"+paramString);
+		
+		try {
+			HttpResponse response = httpclient.execute(httpget);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
 	}
 
 }
